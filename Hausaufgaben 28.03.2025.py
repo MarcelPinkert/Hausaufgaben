c3926f196d0091d1672518f1e222cc484e59c7a3
MarcelPinkert <marcel.pinkert@googlemail.com>
2025-03-28 16:30:27 +0100

    Hausaufgaben 28.03.2025.py

--- Hausaufgaben 28.03.2025.py
+++ Hausaufgaben 28.03.2025.py
@@ -1,15 +1,15 @@
-def create_user_profile(name, email, **kwargs):
+def create_user_profile(name, email, kwargs):
     profile = {
         'name': name,
         'email': email
     }
-
+    
     for key, value in kwargs.items():
         profile[key] = value
-
+        
     return profile
 
-# Beispielaufrufe
+
 alice = create_user_profile(name="Alice", email="alice@example.com", age=33, city="New York")
 bob = create_user_profile(name="Bob", email="bob@example.com", last_name="Smith", age=22, phone_number=1234567890)
 
